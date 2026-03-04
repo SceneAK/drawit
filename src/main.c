31d623c1a3fa20ebf1f5b125b9965b2caa35220c
kueworkspace-admin <36.evan.gunardi@gonzaga.sch.id>
2026-03-05 00:16:35 +0700

    add basic drawing, single strokes

--- src/main.c
+++ src/main.c
@@ -9,10 +9,16 @@
 #include <nanovg/nanovg.h>
 #include <nanovg/nanovg_gl.h>
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
 typedef struct { float x, y; } vec2;
 typedef struct { float x, y, z; } vec3;
+typedef struct { float r, g, b, a; } color;
 
 static float zoomFrac = 0.1f;
+
+static const color CLEAR_COLOR_DEFAULT = { .1f, .1f, .1f, .0f };
+static color clear_color = { .1f, .1f, .1f, 1.0f };
 
 static int screen_width, screen_height;
 static NVGcontext* vg;
@@ -24,6 +30,11 @@ static float zoom = 1.0f;
 static bool is_panning = false;
 static vec2 pan_pivot_mouse;
 static vec2 pan_pivot_camera;
+
+static bool is_drawing = false;
+static const float input_dist_threshold = 1.0f;
+static vec2 drawinput[2048];
+static int drawinput_len = 0;
 
 static inline vec2 screen_to_world(vec2 screen)
 {
@@ -52,6 +63,20 @@ void cleanup()
 	nvgDeleteGL3(vg);
 }
 
+void record_drawinput(vec2 input)
+{
+	vec2 dist;
+
+	if (drawinput_len != 0) {
+		dist.x = input.x - drawinput[drawinput_len-1].x;		
+		dist.y = input.y - drawinput[drawinput_len-1].y;		
+
+		if (dist.x*dist.x + dist.y*dist.y < input_dist_threshold*input_dist_threshold)
+			return;
+	}
+	drawinput[drawinput_len++] = input;
+}
+
 void event(const sapp_event *e)
 {
 	switch(e->type) {
@@ -72,16 +97,27 @@ void event(const sapp_event *e)
 			camera.x = pan_pivot_camera.x + (pan_pivot_mouse.x - mouse_screen.x)/zoom;
 			camera.y = pan_pivot_camera.y + (mouse_screen.y - pan_pivot_mouse.y)/zoom;
 		}
+		if (is_drawing && drawinput_len < ARRAY_SIZE(drawinput)) {
+			record_drawinput(screen_to_world( (vec2) { e->mouse_x, e->mouse_y } ));
+		}
 		break;
 	case SAPP_EVENTTYPE_MOUSE_DOWN:
 		if (e->mouse_button == SAPP_MOUSEBUTTON_MIDDLE) {
 			is_panning = true;
+			clear_color = (color) { 1, 1, 1, 1 };
 			pan_pivot_mouse = mouse_screen;
 			pan_pivot_camera = camera;
+		}
+		if (e->mouse_button == SAPP_MOUSEBUTTON_LEFT && !is_drawing) {
+			is_drawing = true;
+			clear_color = (color) { .15, .15, .15, 1 };
+			drawinput_len = 0;
 		}
 		break;
 	case SAPP_EVENTTYPE_MOUSE_UP:
 		is_panning = is_panning && e->mouse_button != SAPP_MOUSEBUTTON_MIDDLE;
+		is_drawing = is_drawing && e->mouse_button != SAPP_MOUSEBUTTON_LEFT;
+		clear_color = CLEAR_COLOR_DEFAULT;
 		break;
 	case SAPP_EVENTTYPE_MOUSE_SCROLL:
 		float ratio = (1 + zoomFrac * e->scroll_y);
@@ -98,13 +134,11 @@ void event(const sapp_event *e)
 
 void frame(void) 
 {
+	int i;
 	float dpi = sapp_dpi_scale();
 
 	glViewport(0, 0, screen_width, screen_height);
-	if (is_panning)
-		glClearColor(1, 1, 1, 1.0f);
-	else
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	glClearColor(clear_color.r, clear_color.g, clear_color.b, clear_color.a);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
 	nvgBeginFrame(vg, screen_width/dpi, screen_height/dpi, dpi);
@@ -116,6 +150,18 @@ void frame(void)
 			nvgRect(vg, -25, -25, 50, 50);
 		nvgFillColor(vg, nvgRGBA(255, 192, 0, 255));
 		nvgFill(vg);
+
+		if (is_drawing) {
+			nvgBeginPath(vg);
+			nvgStrokeWidth(vg, 3);
+			nvgLineCap(vg, NVG_ROUND);
+			nvgLineJoin(vg, NVG_ROUND);
+			nvgStrokeColor(vg, nvgRGBA(204, 255, 0, 255));
+				nvgMoveTo(vg, drawinput[0].x, -drawinput[0].y);
+				for (i = 1; i < drawinput_len; i++)
+					nvgLineTo(vg, drawinput[i].x, -drawinput[i].y);
+			nvgStroke(vg);
+		}
 
 	nvgEndFrame(vg);
 }
